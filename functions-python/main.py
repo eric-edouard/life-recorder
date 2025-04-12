78d394d5321d54331b771ffcbd5887080184a3c4
Eric Edouard <eamilhat@gmail.com>
2025-04-12 11:48:22 +0200

    Implement Cloud Function to log archived storage objects and update requirements

--- functions-python/main.py
+++ functions-python/main.py
@@ -2,13 +2,21 @@
 # To get started, simply uncomment the below code or create your own.
 # Deploy with `firebase deploy`
 
-from firebase_functions import https_fn
-from firebase_admin import initialize_app
+from firebase_functions import storage_fn, logger
+from firebase_admin import initialize_app, storage
+import pathlib
+
+initialize_app()
 
 
-# initialize_app()
-#
-#
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
+@storage_fn.on_object_archived()
+def log_object_archived(event: storage_fn.CloudEvent[storage_fn.StorageObjectData]):
+    bucket_name = event.data.bucket
+    file_path = pathlib.PurePath(event.data.name)
+    content_type = event.data.content_type
+
+    logger.info(f"Bucket: {bucket_name}")
+    logger.info(f"File: {file_path}")
+    logger.info(f"Content type: {content_type}")
+
+    return
