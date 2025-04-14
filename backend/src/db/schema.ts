201cddb6ccc089772282c1cb17881f63b9566ce1
Eric Edouard <eamilhat@gmail.com>
2025-04-14 13:58:49 +0200

    Add location, latitude, and longitude fields to memoriesTable in schema.ts

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,7 +1,16 @@
-import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import {
+	decimal,
+	integer,
+	pgTable,
+	text,
+	timestamp,
+} from "drizzle-orm/pg-core";
 
 export const memoriesTable = pgTable("memories", {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(),
 	content: text().notNull(),
+	location: text(),
+	latitude: decimal({ precision: 10, scale: 8 }),
+	longitude: decimal({ precision: 11, scale: 8 }),
 	createdAt: timestamp().notNull().defaultNow(),
 });
