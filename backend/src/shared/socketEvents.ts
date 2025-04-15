98443418d0b2f508089548b4ef2dacc67b51bd6f
Eric Edouard <eamilhat@gmail.com>
2025-04-15 10:37:54 +0200

    Add prebuild script to copy shared files and update tsconfig for rootDir

---
+++ backend/src/shared/socketEvents.ts
@@ -0,0 +1,18 @@
+export type ServerLog = {
+	type: "log" | "warn" | "error";
+	message: string;
+	timestamp: number;
+};
+
+export interface ServerToClientEvents {
+	serverLog: (logData: ServerLog) => void;
+}
+
+export interface ClientToServerEvents {
+	audioData: (
+		data: { packets: number[][]; timestamp: number },
+		callback: (success: boolean) => void,
+	) => void;
+	startLogForwarding: (callback: (success: boolean) => void) => void;
+	stopLogForwarding: (callback: (success: boolean) => void) => void;
+}
