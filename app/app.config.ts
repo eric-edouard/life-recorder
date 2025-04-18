28ffaa1adc7ee828378729ac1be77cbcdb1755d8
Eric Edouard <eamilhat@gmail.com>
2025-04-18 14:39:58 +0200

    Update app version to 1.0.1 in app.config.ts and modify package.json to set APP_VARIANT for development in start script.

--- app/app.config.ts
+++ app/app.config.ts
@@ -21,7 +21,7 @@ export default ({ config }: ConfigContext) => ({
 	expo: {
 		name: getAppName(),
 		slug: "life-recorder",
-		version: "1.0.0",
+		version: "1.0.1",
 		orientation: "portrait",
 		icon: "./assets/images/icon.png",
 		scheme: "life-recorder",
