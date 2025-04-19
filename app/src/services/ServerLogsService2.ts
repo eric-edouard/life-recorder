4c33c056c74c0bb4b83b535deb114a976c0c5a42
Eric Edouard <eamilhat@gmail.com>
2025-04-19 16:21:45 +0200

    Add ServerLogsService to handle server log events with socket integration

--- app/src/services/ServerLogsService2.ts
+++ app/src/services/ServerLogsService2.ts
@@ -2,48 +2,41 @@ import type { ServerLog } from "@/src/shared/socketEvents";
 import { observable } from "@legendapp/state";
 import { socketService } from "./socketService";
 
-export const serverLogsService = (() => {
-	const isListening = observable(false);
-	const logs$ = observable<ServerLog[]>([]);
+class ServerLogsService {
+	public isListening = observable(false);
+	public logs$ = observable<ServerLog[]>([]);
 
-	// Initialize socket listeners
-	(() => {
-		// Get the socket from the socket service
+	constructor() {
+		// Initialize socket listeners
 		const socket = socketService.getSocket();
 
 		socket.on("serverLog", (log) => {
 			// Get current logs and append the new log
-			logs$.set((currentLogs) => [...currentLogs, log]);
+			this.logs$.set((currentLogs) => [...currentLogs, log]);
 		});
-	})();
+	}
 
-	const startListeningToServerLogs = () => {
+	startListeningToServerLogs = () => {
 		const socket = socketService.getSocket();
 
 		socket.emit("startLogForwarding", () => {
-			isListening.set(true);
+			this.isListening.set(true);
 			console.log("Started listening to server logs");
 		});
 	};
 
-	const stopListeningToServerLogs = () => {
+	stopListeningToServerLogs = () => {
 		const socket = socketService.getSocket();
 
 		socket.emit("stopLogForwarding", () => {
-			isListening.set(false);
+			this.isListening.set(false);
 			console.log("Stopped listening to server logs");
 		});
 	};
 
-	const clearLogs = () => {
-		logs$.set([]);
+	clearLogs = () => {
+		this.logs$.set([]);
 	};
+}
 
-	return {
-		isListening,
-		logs$,
-		startListeningToServerLogs,
-		stopListeningToServerLogs,
-		clearLogs,
-	};
-})();
+export const serverLogsService = new ServerLogsService();
