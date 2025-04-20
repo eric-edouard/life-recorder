39563d1497b1902e6d79065976f1c1c3bd1b2677
Eric Edouard <eamilhat@gmail.com>
2025-04-20 13:01:50 +0200

    Integrate react-navigation theme provider into ThemeContext for dynamic theming; customize navigation theme based on color scheme.

--- app/src/contexts/ThemeContext.tsx
+++ app/src/contexts/ThemeContext.tsx
@@ -3,6 +3,12 @@ import {
 	customColors,
 	customColorsVars,
 } from "@/src/constants/customColors";
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider as NavigationThemeProvider,
+	type Theme,
+} from "@react-navigation/native";
 import type React from "react";
 import { createContext, useContext } from "react";
 import { View, useColorScheme } from "react-native";
@@ -20,14 +26,30 @@ export const ThemeProvider = ({ children }: ThemeProviderProps) => {
 	const colorScheme = useColorScheme();
 	const currentThemeColors = useCurrentColorsVariants();
 
+	const customiseNavigationTheme = (t: Theme) => {
+		return {
+			...t,
+			colors: {
+				...t.colors,
+				primary: customColors[colorScheme ?? "dark"]["--accent"],
+			},
+		} as Theme;
+	};
+
 	return (
 		<ThemeContext.Provider value={{ theme: colorScheme ?? "dark" }}>
-			<View
-				style={[customColorsVars[colorScheme ?? "dark"], currentThemeColors]}
-				className="flex-1"
+			<NavigationThemeProvider
+				value={customiseNavigationTheme(
+					colorScheme === "dark" ? DarkTheme : DefaultTheme,
+				)}
 			>
-				{children}
-			</View>
+				<View
+					style={[customColorsVars[colorScheme ?? "dark"], currentThemeColors]}
+					className="flex-1"
+				>
+					{children}
+				</View>
+			</NavigationThemeProvider>
 		</ThemeContext.Provider>
 	);
 };
