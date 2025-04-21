bbef7c8cea5dc5a0b7cb4d657a9efb75e57dab14
Eric Edouard <eamilhat@gmail.com>
2025-04-21 06:42:08 +0200

    Refactor Tailwind configuration for improved readability; maintain existing debug utilities and structure.

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -3,43 +3,45 @@ import { withAccentColor } from "./utils/withAccentColor";
 const { hairlineWidth } = require("nativewind/theme");
 
 /** @type {import('tailwindcss').Config} */
-module.exports = withAccentColor(withUIKit({
-	content: ["./src/**/*.{js,jsx,ts,tsx}"],
-	presets: [require("nativewind/preset")],
-	theme: {
-		extend: {
-			height: {
-				row: 44,
-			},
-			padding: {
-				sm: 8,
-				md: 16,
-				lg: 20
+module.exports = withAccentColor(
+	withUIKit({
+		content: ["./src/**/*.{js,jsx,ts,tsx}"],
+		presets: [require("nativewind/preset")],
+		theme: {
+			extend: {
+				height: {
+					row: 44,
+				},
+				padding: {
+					sm: 8,
+					md: 16,
+					lg: 20,
+				},
 			},
 		},
-	},
-	plugins: [
-		({ addComponents }) => {
-			/**
-			 * Debug utilities
-			 */
-			addComponents({
-				".d": {
-					borderWidth: "1px",
-					borderColor: "red",
-					borderStyle: "solid",
-				},
-				".d1": {
-					borderWidth: "1px",
-					borderColor: "blue",
-					borderStyle: "solid",
-				},
-				".d2": {
-					borderWidth: "1px",
-					borderColor: "yellow",
-					borderStyle: "solid",
-				},
-			});
-		},
-	],
-}));
+		plugins: [
+			({ addComponents }) => {
+				/**
+				 * Debug utilities
+				 */
+				addComponents({
+					".d": {
+						borderWidth: "1px",
+						borderColor: "red",
+						borderStyle: "solid",
+					},
+					".d1": {
+						borderWidth: "1px",
+						borderColor: "blue",
+						borderStyle: "solid",
+					},
+					".d2": {
+						borderWidth: "1px",
+						borderColor: "yellow",
+						borderStyle: "solid",
+					},
+				});
+			},
+		],
+	}),
+);
