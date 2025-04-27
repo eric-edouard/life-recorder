ac7a1d8e89a7a1314e366b77e9d7c2fb3b86cc36
Eric Edouard <eamilhat@gmail.com>
2025-04-27 11:46:47 +0200

    Add getSignedUrl utility for generating signed URLs for GCS files, enabling secure access to recordings.

---
+++ backend/src/utils/gcs/getSignedUrl.ts
@@ -0,0 +1,14 @@
+import {
+	RECORDINGS_FOLDER,
+	gcsBucket,
+} from "@backend/src/services/external/gcs";
+
+export const getSignedUrl = async (filename: string) => {
+	const file = gcsBucket.file(`${RECORDINGS_FOLDER}/${filename}`);
+	const [url] = await file.getSignedUrl({
+		version: "v4",
+		action: "read",
+		expires: Date.now() + 60 * 60 * 1000, // 1 hour
+	});
+	return url;
+};
